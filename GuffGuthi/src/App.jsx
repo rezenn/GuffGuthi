81ef76625b378d6815b66df2078079fd8a20349f
rezen <rezen.khadgi@gmail.com>
2024-12-29 13:31:18 +0545

    view profile

--- GuffGuthi/src/App.jsx
+++ GuffGuthi/src/App.jsx
@@ -3,18 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import ForgotPassword from "./pages/ForgotPassword"
+import Profile from "./components/Profile";
 
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/forgotPassword" element={<ForgotPassword />} />
-      </Routes>
-    </Router>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<Login />} />
+    //     <Route path="/login" element={<Login />} />
+    //     <Route path="/register" element={<Register />} />
+    //     <Route path="/forgotPassword" element={<ForgotPassword />} />
+    //   </Routes>
+    // </Router>
+    <Profile/>
   );
 }
 
