745ab027da818fe33e5717b9f37973d582f6e9c5
aryan123jung <149761592+aryan123jung@users.noreply.github.com>
2024-12-30 12:45:47 +0545

    post

--- GuffGuthi/src/App.jsx
+++ GuffGuthi/src/App.jsx
@@ -1,21 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
-import ForgotPassword from "./pages/ForgotPassword"
+import ForgotPassword from "./pages/ForgotPassword";
+import Home from "./pages/home"; // Vertical navbar
 
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/forgotPassword" element={<ForgotPassword />} />
-      </Routes>
-    </Router>
-  );
+  const [activePage, setActivePage] = useState("home");
+
+  const renderContent = () => {
+    switch (activePage) {
+      case "home":
+        return <div>Welcome to the Home Page</div>;
+      case "popular":
+        return <div>Popular Posts Section</div>;
+      case "my-posts":
+        return <div>My Posts Section</div>;
+      case "requests":
+        return <div>Requests Section</div>;
+      case "chat":
+        return <div>Chat Section</div>;
+      default:
+        return <div>Welcome to the Home Page</div>;
+    }
+  };
+
+//   return (
+//     <Router>
+//       <Routes>
+//         {/* Authentication Routes */}
+//         <Route path="/" element={<Login />} />
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/register" element={<Register />} />
+//         <Route path="/forgotPassword" element={<ForgotPassword />} />
+
+//         {/* Protected Routes */}
+//         <Route
+//           path="/*"
+//           element={
+//             <div style={{ display: "flex" }}>
+//               {/* Vertical Navbar */}
+//               <Home activePage={activePage} setActivePage={setActivePage} />
+
+//               {/* Main Content Area */}
+//               <div style={{ marginLeft: "200px", padding: "20px", flexGrow: 1 }}>
+//                 {renderContent()}
+//               </div>
+//             </div>
+//           }
+//         />
+//       </Routes>
+//     </Router>
+//   );
+return(<Home/>) 
 }
 
 export default App;
