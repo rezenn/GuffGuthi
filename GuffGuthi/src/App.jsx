ba8aa71629fe2f2a72f60e3f39d8232e48f8ce50
sudip <chsudip30@gmail.com>
2025-01-05 09:49:25 +0545

    Text Editor

--- GuffGuthi/src/App.jsx
+++ GuffGuthi/src/App.jsx
@@ -12,50 +12,61 @@ import ViewPost from "./components/profileComponents/ViewPost";
 import CoverImage from "./components/profileComponents/CoverImage";
 import EditProfilePage from "./pages/EditProfilePage";
 import CreatePost from "./pages/CreatePost";
+import HtmlEditor from "./components/TextEditor/HtmlEditor";
+import PostRequest from "./pages/PostRequest";
 
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/forgotPassword" element={<ForgotPassword />} />
-      </Routes>
-    </Router> 
-    // <Router>
-    //   <Routes>
-    //     <Route path="/" element={<ViewProfilePage />} /> 
-    //     <Route path="/viewProfilePage" element={<ViewProfilePage />} /> 
-    //     <Route path="/editProfilePage" element={<EditProfilePage />} /> 
-    //   </Routes>
-    // </Router> 
-  );
   const [activePage, setActivePage] = useState("Navbar");
 
-  // const renderContent = () => {
-  //   switch (activePage) {
-  //     case "home":
-  //       return <div>Welcome to the Home Page</div>;
-  //     case "popular":
-  //       return <div>Popular Posts Section</div>;
-  //     case "my-posts":
-  //       return <div>My Posts Section</div>;
-  //     case "requests":
-  //       return <div>Requests Section</div>;
-  //     case "chat":
-  //       return <div>Chat Section</div>;
-  //     default:
-  //       return <div>Welcome to the Home Page</div>;
-  //   }
-  // };
+  const renderContent = () => {
+    switch (activePage) {
+      case "home":
+        return <div>Welcome to the Home Page</div>;
+      case "popular":
+        return <div>Popular Posts Section</div>;
+      case "my-posts":
+        return <div>My Posts Section</div>;
+      case "requests":
+        return <div>Requests Section</div>;
+      case "chat":
+        return <div>Chat Section</div>;
+      default:
+        return <div>Welcome to the Home Page</div>;
+    }
+  };
 
-// return(<>
-// {/* <EditProfilePage/> */}
-// {/* <ViewProfilePage/> */}
-// </>  
-// ) 
+//   return (
+//     <Router>
+//       <Routes>
+//         {/* Authentication Routes */}
+//         <Route path="/" element={<Login />} />
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/register" element={<Register />} />
+//         <Route path="/forgotPassword" element={<ForgotPassword />} />
+
+//         {/* Protected Routes */}
+//         <Route
+//           path="/*"
+//           element={
+//             <div style={{ display: "flex" }}>
+//               {/* Vertical Navbar */}
+//               <Home activePage={activePage} setActivePage={setActivePage} />
+
+//               {/* Main Content Area */}
+//               <div style={{ marginLeft: "200px", padding: "20px", flexGrow: 1 }}>
+//                 {renderContent()}
+//               </div>
+//             </div>
+//           }
+//         />
+//       </Routes>
+//     </Router>
+//   );
+// return(<CreatePost/>) 
+
+return(<CreatePost/>) 
+// return(<HtmlEditor/>)
 }
 
 export default App;
--- GuffGuthi/src/pages/CreatePost.jsx
+++ GuffGuthi/src/pages/CreatePost.jsx
@@ -1,34 +1,40 @@
 import React from 'react';
 import Navbar from '../components/navbar/Navbar';
-import './CreatePost.css'
-
+import './CreatePost.css';
+import HtmlEditor from '../components/TextEditor/HtmlEditor';
 
 const CreatePost = () => {
-    return (
 
-        <>
-        <Navbar/>
-        <div className='CreatePost-Container'>
 
-            <h1>Create Post</h1>
-            <input type="text" 
+  return (
+    <>
+      <Navbar />
+      <HtmlEditor className="Editor"/>
+     
+      <div className='CreatePost-Container'>
+        <h1>Create Post</h1>
+
+        <input
+          type='text'
+          className='title'
+          placeholder='Title'
+        />
+
+
             
-            className="title"
-            placeholder="Title"/>
-
-
-            <textarea type="text"
+          {/* <textarea type="text"
             className='BodyText' 
-            />
-<div className='ButtonPosition'><button className='Post-Button'>Post</button></div>
-            
-            
+            /> */}
+
+        <div className='ButtonPosition'>
+          <button className='Post-Button'>Post</button>
         </div>
-
-
-
-        </>
-    );
+      </div>
+    </>
+  );
 };
 
 export default CreatePost;
+
+
+          
