dd860e4f878be445938501b89644a147c9a1eed0
rezen <rezen.khadgi@gmail.com>
2025-01-07 05:40:42 +0545

    bug fix

--- GuffGuthi/src/App.jsx
+++ GuffGuthi/src/App.jsx
@@ -24,6 +24,7 @@ function App() {
         <Route path="/forgotPassword" element={<ForgotPassword />} />
       </Routes>
     </Router> 
+    //
     // <Router>
     //   <Routes>
     //     <Route path="/" element={<ViewProfilePage />} /> 
