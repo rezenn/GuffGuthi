4590696ff4b342c198ef2012965291e7c1d35452
rezen <rezen.khadgi@gmail.com>
2025-01-07 12:38:43 +0545

    error  fix

--- GuffGuthi/src/components/profileComponents/CoverImage.jsx
+++ GuffGuthi/src/components/profileComponents/CoverImage.jsx
@@ -3,8 +3,10 @@ import './coverImage.css'
 function CoverImage(){
     return<>
     <div>
+        {/*  */}
         <img src="./src/assets/SwayambuCover.jpg" alt="Cover image" />
     </div>
     </>
 }
+//
 export default CoverImage;
