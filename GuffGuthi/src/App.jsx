53ca59994e9ff57a943e31a9cdce02562d48d0c6
rezen <rezen.khadgi@gmail.com>
2025-01-07 12:08:34 +0545

    bug fix

--- GuffGuthi/src/App.jsx
+++ GuffGuthi/src/App.jsx
@@ -3,62 +3,39 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import ForgotPassword from "./pages/ForgotPassword";
+import Profile from "./components/profileComponents/Profile";
+import EditProfile from "./components/profileComponents/EditProfile";
 import Navbar from "./components/navbar/Navbar";
 import ViewProfilePage from "./pages/ViewProfilePage";
-import home from "./pages/home";
+import ViewPost from "./components/profileComponents/ViewPost";
+import CoverImage from "./components/profileComponents/CoverImage";
+import EditProfilePage from "./pages/EditProfilePage";
 import CreatePost from "./pages/CreatePost";
-
+import Post from "./components/postINhome/Post";
+import Chat from "./components/chatINhome/Chat";
 
 function App() {
-  const [activePage, setActivePage] = useState("Navbar");
+  const [activePage, setActivePage] = useState("home");
 
-  const renderContent = () => {
-    switch (activePage) {
-      case "home":
-        return <div>Welcome to the Home Page</div>;
-      case "popular":
-        return <div>Popular Posts Section</div>;
-      case "my-posts":
-        return <div>My Posts Section</div>;
-      case "requests":
-        return <div>Requests Section</div>;
-      case "chat":
-        return <div>Chat Section</div>;
-      default:
-        return <div>Welcome to the Home Page</div>;
-    }
-  };
+  return (
+    <Router>
+      {/* Render Navbar conditionally */}
+      {window.location.pathname !== "/login" && window.location.pathname !== "/register" && (
+        <Navbar activePage={activePage} setActivePage={setActivePage} />
+      )}
 
-//   return (
-//     <Router>
-//       <Routes>
-//         {/* Authentication Routes */}
-//         <Route path="/" element={<Login />} />
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/register" element={<Register />} />
-//         <Route path="/forgotPassword" element={<ForgotPassword />} />
-
-//         {/* Protected Routes */}
-//         <Route
-//           path="/*"
-//           element={
-//             <div style={{ display: "flex" }}>
-//               {/* Vertical Navbar */}
-//               <Home activePage={activePage} setActivePage={setActivePage} />
-
-//               {/* Main Content Area */}
-//               <div style={{ marginLeft: "200px", padding: "20px", flexGrow: 1 }}>
-//                 {renderContent()}
-//               </div>
-//             </div>
-//           }
-//         />
-//       </Routes>
-//     </Router>
-//   );
-//return(<Navbar/>) 
-
-rerturn(<CreatePost/>)
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgotPassword" element={<ForgotPassword />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/createPost" element={<CreatePost />} />
+        <Route path="/viewProfilePage" element={<ViewProfilePage />} />
+        <Route path="/editProfilePage" element={<EditProfilePage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
