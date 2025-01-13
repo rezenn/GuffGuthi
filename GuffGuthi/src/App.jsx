fb4083364ea2d8852548ff961e6a8e9d8941bc66
sudip <chsudip30@gmail.com>
2025-01-13 20:58:17 +0545

    Group Feed Page Setup

--- GuffGuthi/src/App.jsx
+++ GuffGuthi/src/App.jsx
@@ -7,7 +7,7 @@ import Profile from "./components/profileComponents/Profile";
 import EditProfile from "./components/profileComponents/EditProfile";
 import Navbar from "./components/navbar/Navbar";
 import ViewProfilePage from "./pages/ViewProfilePage";
-import Home from "./pages/Home";
+import Home from "./pages/home";
 import ViewPost from "./components/profileComponents/ViewPost";
 import CoverImage from "./components/profileComponents/CoverImage";
 import EditProfilePage from "./pages/EditProfilePage";
@@ -17,29 +17,31 @@ import Chat from "./components/chatINhome/Chat";
 import PostRequest from "./pages/PostRequest/PostRequest";
 import HtmlEditor from "./components/TextEditor/HtmlEditor";
 import CommunityServices from "./components/REQUEST/Request";
+import GroupFeedPage from "./pages/GroupFeedPage";
 function App() {
   // const [activePage, setActivePage] = useState("home");
 
   return (
-    <Router>
-      {/* Render Navbar conditionally */}
-      {/* {window.location.pathname !== "/login" && window.location.pathname !== "/register" && (
-        // <Navbar activePage={activePage} setActivePage={setActivePage} />
-      )} */}
+    // <Router>
+    //   {/* Render Navbar conditionally */}
+    //   {/* {window.location.pathname !== "/login" && window.location.pathname !== "/register" && (
+    //     // <Navbar activePage={activePage} setActivePage={setActivePage} />
+    //   )} */}
 
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/forgotPassword" element={<ForgotPassword />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/createPost" element={<CreatePost />} />
-        <Route path="/viewProfilePage" element={<ViewProfilePage />} />
-        <Route path="/editProfilePage" element={<EditProfilePage />} />
-        <Route path="/communityServices" element={<CommunityServices />} />
-        <Route path="/postRequest" element={<PostRequest />} />
-      </Routes>
-    </Router>
+    //   <Routes>
+    //     <Route path="/" element={<Login />} />
+    //     <Route path="/login" element={<Login />} />
+    //     <Route path="/register" element={<Register />} />
+    //     <Route path="/forgotPassword" element={<ForgotPassword />} />
+    //     <Route path="/home" element={<Home />} />
+    //     <Route path="/createPost" element={<CreatePost />} />
+    //     <Route path="/viewProfilePage" element={<ViewProfilePage />} />
+    //     <Route path="/editProfilePage" element={<EditProfilePage />} />
+    //     <Route path="/communityServices" element={<CommunityServices />} />
+    //     <Route path="/postRequest" element={<PostRequest />} />
+    //   </Routes>
+    // </Router>
+    <GroupFeedPage/>
 
 
   );
